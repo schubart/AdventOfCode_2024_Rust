7762ca6b98b163ce8095d2c8ce6761705d92b007
Michael Schubart <michael@schubart.net>
2025-04-10 18:52:48 +0100

    Fix Clippy warnings

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -43,7 +43,7 @@ fn visit(grid: &Grid, start: Position) -> Positions {
             _ => {
                 return visited;
             }
-        };
+        }
     }
 }
 
@@ -73,7 +73,7 @@ fn is_loop(grid: &Grid, start: Position, obstacle: Position) -> bool {
             _ => {
                 return false;
             }
-        };
+        }
     }
 }
 
